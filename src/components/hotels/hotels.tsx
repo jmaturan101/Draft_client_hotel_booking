c3c2928c374dfe152c86c6e0029b37e7f4af3163
John Maturan <jmaturan@deloitte.com>
2021-09-21 15:36:22 -0400

    frontend is developed

--- src/components/hotels/hotels.tsx
+++ src/components/hotels/hotels.tsx
@@ -2,13 +2,15 @@ import { Component } from 'react';
 import { Hotel } from '../../model/model';
 import { DataService } from '../../services/DataService';
 import { HotelComponent } from './HotelComponent';
-
+import { ConfromModalComponent } from './ConfirmModalComponent';
 interface HotelsState {
-    hotels: Hotel[]
+    hotels: Hotel[],
+    showModal: boolean,
+    modalContent: string
 }
 
 interface HotelsProps {
-    dataService: DataService
+    dataService: DataService,
 }
 
 export class Hotels extends Component<HotelsProps, HotelsState> {
@@ -16,9 +18,13 @@ export class Hotels extends Component<HotelsProps, HotelsState> {
     constructor(props: HotelsProps) {
         super(props)
         this.state = {
-            hotels: []
+            hotels: [],
+            showModal: false,
+            modalContent: ''
+
         }
         this.reserveHotel = this.reserveHotel.bind(this);
+        this.closeModal = this.closeModal.bind(this)
     }
 
     async componentDidMount() {
@@ -28,7 +34,20 @@ export class Hotels extends Component<HotelsProps, HotelsState> {
         });
     }
 
-    private async reserveHotel(hotelId: string) { }
+    private async reserveHotel(hotelId: string) {
+        const reservationResult = await this.props.dataService.reserveHotels(hotelId)
+        if (reservationResult) {
+            this.setState({
+                showModal: true,
+                modalContent: `You reserved the space with id ${hotelId} and got the reservation number ${reservationResult}`
+            })
+        } else {
+            this.setState({
+                showModal: true,
+                modalContent: `You can't reserve the space with id ${hotelId}`
+            })
+        }
+     }
 
     private renderHotels() {
         const rows: any[] = []
@@ -46,11 +65,22 @@ export class Hotels extends Component<HotelsProps, HotelsState> {
         return rows;
     }
 
+    private closeModal() {
+        this.setState({
+            showModal: false,
+            modalContent: ''
+        })
+    }
+
     render() {
         return (
             <div>
                 <h2>Welcome to the Hotels page!</h2>
                 {this.renderHotels()}
+                <ConfromModalComponent
+                    close={this.closeModal}
+                    content={this.state.modalContent}
+                    show={this.state.showModal} />
             </div>
         )
     }
--- src/services/DataService.ts
+++ src/services/DataService.ts
@@ -1,6 +1,9 @@
 import { Hotel } from "../model/model";
 
 export class DataService {
+    reserveHotel(hotelId: string) {
+        throw new Error('Method not implemented.');
+    }
 
     public async getHotels(): Promise<Hotel[]> {
         const result: Hotel[] = []
@@ -21,4 +24,13 @@ export class DataService {
         });
         return result;
     }
-} 
+
+public async reserveHotels(hotelId: string):Promise<string | undefined> {
+    if (hotelId === '123') {
+        return('5555')
+    } else {
+        return undefined
+    }
+}
+}
+
