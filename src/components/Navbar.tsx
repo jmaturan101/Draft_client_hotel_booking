bcd068660e119ac01f1335b7a4ed70b0b4f953db
Maturan <jmaturan@deloitte.com>
2021-09-21 11:53:22 -0400

    Routing

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,25 +1,28 @@
 import React from "react";
 import { User } from "../model/model";
-import { Link } from 'react-router-dom';
+import { Link } from 'react-router-dom'
 
-export class Navbar extends React.Component <{
+
+
+export class Navbar extends React.Component<{
     user: User | undefined
 }> {
 
-    render() {
-        let loginLogOut : any = this.props
+    render(){
+        let loginLogOut: any
         if (this.props.user) {
             loginLogOut = <Link to='/logout'>{this.props.user.userName}</Link>
         } else {
             loginLogOut = <Link to='/login'>Login</Link>
         }
 
-        return (
-            <div className='nacbar'>
-                <Link to='/'>Home</Link>
-                <Link to='/profile'>Home</Link>
+
+        return(
+            <div className='navbar'>
+                <Link to='/'> Home </Link>
+                <Link to='/profile'> Profile</Link>
                 {loginLogOut}
             </div>
         )
     }
-}
+} 
